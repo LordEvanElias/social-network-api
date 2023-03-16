f44d13d245a29d6bf754a17f376c9ef2d382a9b5
Lord Evan <114256148+LordEvanElias@users.noreply.github.com>
2023-03-15 20:34:58 -0500

    Fixed a bug where the console thunderclient returned null when posting a thought.

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -24,6 +24,9 @@ module.exports = {
           { new: true }
         );
       })
+      .then(() => {
+        res.json({ message: "Thought Posted!" });
+      })
       .then((dbUserData) => res.json(dbUserData))
       .catch((err) => res.status(500).json(err));
   },
